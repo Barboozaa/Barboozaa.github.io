ee983db3d26d97da44a1991e174ca44fe7d3214b
inkspot <Wyattsimpson777@gmail.com>
2018-07-23 19:48:38 -0400

    added stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
-// Hello
+$(document).ready(function() {
+    $(".projects").hide();
+    $(".contactMe").hide();
+    var projectsHidden = true;
+    var contactMeHidden = true;
+
+    $("#projectLink").click(function() {
+        if (projectsHidden == true) {
+            projectsHidden = false
+            $(".projects").show();
+        } else return false
+    });
+
+    $("#contact").click(function() {
+        if (contactMeHidden == true) {
+            contactMeHidden = false
+            $(".contactMe").show();
+        } else return false
+    })
+});
